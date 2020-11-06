76ca0f348af6a547ae4d474b263084eec937b7a6
emPeeGee <eempeegee@gmail.com>
2020-11-06 18:11:09 +0200

    fix: Corect some css and add images to project

--- src/app/views/partners/partners.component.ts
+++ src/app/views/partners/partners.component.ts
@@ -11,13 +11,13 @@ export class PartnersComponent implements OnInit {
     {
       logos: [
         'assets/images/gazprom.png',
-        'assets/images/vodofone.png',
+        'assets/images/vodafone.png',
         'assets/images/toyota.png',
       ],
       images: [
-        'https://images.unsplash.com/photo-1548337138-e87d889cc369?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9',
-        'https://russiabusinesstoday.com/wp-content/uploads/2018/07/gas-pipeline.jpg',
-        'https://www.nsenergybusiness.com/wp-content/uploads/sites/3/2019/12/pipelines-1632712-740x520.jpg'
+        'assets/images/background_gas_pipeline.jpg',
+        'assets/images/background_pipelines.jpg',
+        'assets/images/background_wind_turbines.jpg',
       ],
       elems: 3
     },
@@ -25,12 +25,12 @@ export class PartnersComponent implements OnInit {
       logos: [
         'assets/images/gazprom.png',
         'assets/images/toyota.png',
-        'assets/images/vodofone.png',
+        'assets/images/vodafone.png',
       ],
       images: [
-        'https://russiabusinesstoday.com/wp-content/uploads/2018/07/gas-pipeline.jpg',
-        'https://www.nsenergybusiness.com/wp-content/uploads/sites/3/2019/12/pipelines-1632712-740x520.jpg',
-        'https://images.unsplash.com/photo-1548337138-e87d889cc369?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9',
+        'assets/images/background_gas_pipeline.jpg',
+        'assets/images/background_wind_turbines.jpg',
+        'assets/images/background_pipelines.jpg',
       ],
       elems: 3,
     },
