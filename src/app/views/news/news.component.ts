0b4a3a2f5e00439f06c035c8788de5d3c69b3f73
emPeeGee <eempeegee@gmail.com>
2020-11-07 17:27:19 +0200

    refactor: Stucture images correctly

--- src/app/views/news/news.component.ts
+++ src/app/views/news/news.component.ts
@@ -12,19 +12,19 @@ export class NewsComponent implements OnInit {
 
   slides: News[] = [
     {
-      image: 'assets/images/news_daily.png',
+      image: 'assets/images/backgrounds/news_daily.png',
       date: new Date(2020, 5, 17),
       title: 'Daily (24.05.2020): EUAs surpassed the $ 25-level for the first time in four months on Tuesday amid bullish economic data',
       description: 'Crude oil futures pulled back off the highest level since before coronavirus pandemic on Tuesday amid a sharp increase in COVID-19 cases and before the release of a weekly report which is expected to reveal that crude inventories have reached a new all-time high in the United States.'
     },
     {
-      image: 'https://i.pinimg.com/originals/bf/82/f6/bf82f6956a32819af48c2572243e8286.jpg',
+      image: 'assets/images/backgrounds/cyberpunk_background1.jpg',
       date: new Date(2020, 5, 17),
       title: 'Daily (24.05.2020): EUAs surpassed the $ 25-level for the first time in four months on Tuesday amid bullish economic data',
       description: 'Crude oil futures pulled back off the highest level since before coronavirus pandemic on Tuesday amid a sharp increase in COVID-19 cases and before the release of a weekly report which is expected to reveal that crude inventories have reached a new all-time high in the United States.'
     },
     {
-      image: 'https://paranoidandroid.co/assets/wallpapers/2018/submerged_desktop_thumb.jpg',
+      image: 'assets/images/backgrounds/cyberpunk_background2.jpg',
       date: new Date(2020, 5, 17),
       title: 'Daily (24.05.2020): EUAs surpassed the $ 25-level for the first time in four months on Tuesday amid bullish economic data',
       description: 'Crude oil futures pulled back off the highest level since before coronavirus pandemic on Tuesday amid a sharp increase in COVID-19 cases and before the release of a weekly report which is expected to reveal that crude inventories have reached a new all-time high in the United States.'
--- src/app/views/partners/partners.component.ts
+++ src/app/views/partners/partners.component.ts
@@ -10,40 +10,40 @@ export class PartnersComponent implements OnInit {
   slides = [
     {
       logos: [
-        'assets/images/gazprom.png',
-        'assets/images/vodafone.png',
-        'assets/images/toyota.png',
+        'assets/images/logos/gazprom.png',
+        'assets/images/logos/vodafone.png',
+        'assets/images/logos/toyota.png',
       ],
       images: [
-        'assets/images/background_gas_pipeline.jpg',
-        'assets/images/background_pipelines.jpg',
-        'assets/images/background_wind_turbines.jpg',
+        'assets/images/backgrounds/background_gas_pipeline.jpg',
+        'assets/images/backgrounds/background_pipelines.jpg',
+        'assets/images/backgrounds/background_wind_turbines.jpg',
       ],
       elems: 3
     },
     {
       logos: [
-        'assets/images/toyota.png',
-        'assets/images/gazprom.png',
-        'assets/images/vodafone.png',
+        'assets/images/logos/toyota.png',
+        'assets/images/logos/gazprom.png',
+        'assets/images/logos/vodafone.png',
       ],
       images: [
-        'assets/images/background_wind_turbines.jpg',
-        'assets/images/background_pipelines.jpg',
-        'assets/images/background_gas_pipeline.jpg',
+        'assets/images/backgrounds/background_wind_turbines.jpg',
+        'assets/images/backgrounds/background_pipelines.jpg',
+        'assets/images/backgrounds/background_gas_pipeline.jpg',
       ],
       elems: 3,
     },
     {
       logos: [
-        'assets/images/vodafone.png',
-        'assets/images/toyota.png',
-        'assets/images/gazprom.png',
+        'assets/images/logos/vodafone.png',
+        'assets/images/logos/toyota.png',
+        'assets/images/logos/gazprom.png',
       ],
       images: [
-        'assets/images/background_wind_turbines.jpg',
-        'assets/images/background_gas_pipeline.jpg',
-        'assets/images/background_pipelines.jpg',
+        'assets/images/backgrounds/background_wind_turbines.jpg',
+        'assets/images/backgrounds/background_gas_pipeline.jpg',
+        'assets/images/backgrounds/background_pipelines.jpg',
       ],
       elems: 3,
     },
