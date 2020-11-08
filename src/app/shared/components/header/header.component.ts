9757762481967ff983b5c91bd42a9a13d397d687
emPeeGee <eempeegee@gmail.com>
2020-11-08 14:08:53 +0200

    feat: Add animation on faIcon in header when click item

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,19 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { faUser, faMap } from '@fortawesome/free-solid-svg-icons';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations:  [
+    trigger('faAppear', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('200ms ease-in', style({  color: '#83a600', opacity: 1 })),
+      ])
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
   faUser = faUser;
